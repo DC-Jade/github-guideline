a7c4a035b687bb18404ea5bec9185938fa2c579d
DC-Jade <jadebetter8092@gmail.com>
2021-08-06 23:24:37 -0800

    guideline.sh

--- guideline.sh
+++ guideline.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/bash
 echo "# github-guideline" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/DC-Jade/github-guideline.git
-git push -u origin main
+git init #initialize
+git add README.md #add file
+git commit -m "first commit" # commit file; -m refer to log message
+#git branch -M main # change your default branch
+git remote add origin https://github.com/DC-Jade/github-guideline.git # remote add
+git push -u origin master # publishe your file
