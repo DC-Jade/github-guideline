894b3bcb0c414d43a9293792a64983ab55bee290
DC-Jade <jadebetter8092@gmail.com>
2021-08-07 00:12:00 -0800

    guideline update

--- guideline.sh
+++ guideline.sh
@@ -4,5 +4,12 @@ git init #initialize
 git add README.md #add file
 git commit -m "first commit" # commit file; -m refer to log message
 #git branch -M main # change your default branch
-git remote add origin https://github.com/DC-Jade/github-guideline.git # remote add
-git push -u origin master # publishe your file
+git remote add guideline origin https://github.com/DC-Jade/github-guideline.git # remote add
+git push -u guideline master # publishe your file
+
+####################
+# => remote
+####################
+git remote -v # show remote list
+git remote rm guideline # rm remote guideline
+git remote add guideline # add remote guideline
