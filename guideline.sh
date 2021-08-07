410d1fa2c37ffd780d3411832c8f164031fd5714
DC-Jade <jadebetter8092@gmail.com>
2021-08-07 00:56:51 -0800

    test

--- guideline.sh
+++ guideline.sh
@@ -3,9 +3,9 @@ echo "# github-guideline" >> README.md
 git init #initialize
 git add README.md #add file
 git commit -m "first commit" # commit file; -m refer to log message
-#git branch -M main # change your default branch
-git remote add guideline origin https://github.com/DC-Jade/github-guideline.git # remote add
-git push -u guideline master # publishe your file
+git branch -d main # remove your local branch
+git remote add guideline origin https://github.com/DC-Jade/github-guideline.git # add remote
+git push -u guideline master # publish your file at master branch
 
 ####################
 # => remote
@@ -13,3 +13,4 @@ git push -u guideline master # publishe your file
 git remote -v # show remote list
 git remote rm guideline # rm remote guideline
 git remote add guideline # add remote guideline
+git remote set-url test git@github.com:DC-Jade/github-guideline.git
