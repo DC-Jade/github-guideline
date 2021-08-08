c4e4a28bc1f7cdf6062d6699fb4333fb1089e708
DC-Jade <jadebetter8092@gmail.com>
2021-08-07 16:55:56 -0800

    update guideline Sat Aug  7 16:55:56 AKDT 2021

--- guideline.sh
+++ guideline.sh
@@ -3,9 +3,18 @@ echo "# github-guideline" >> README.md
 git init #initialize
 git add README.md #add file
 git commit -m "commit log" # commit file; -m required, refer to log message
-git branch -d main # remove your local branch
+git branch -d test # remove your local branch
 git remote add guideline https://github.com/DC-Jade/github-guideline.git # add remote
 git push -u guideline master # publish your file at master branch
+
+
+####################
+# => branch
+####################
+git checkout <branch name> # change your branch
+git branch -d <branch name> # delete your branch
+
+
 
 ####################
 # => remote
